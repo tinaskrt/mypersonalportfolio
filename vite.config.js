207ce38b5e76170ef6bec4496b8130223046b212
tina <florida.mariacristina@dnsc.edu.ph>
2026-06-09 01:22:10 +0800

    Add Tailwind CSS plugin to Vite config
    Added Tailwind CSS plugin to Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 👈 ADD THIS LINE
 
 // https://vite.dev
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 👈 ADD THIS LINE TO COMPILE TAILWIND V4 STYLES
+  ],
   base: '/mypersonalportfolio/',
 })
