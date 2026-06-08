d9fca7ca1a0289af38ec60a31ff07904a5d917cd
Maria Cristina Florida <florida.mariacristina@dnsc.edu.ph>
2026-06-09 00:56:53 +0800

    fix: add router basename and install package

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { PortfolioProvider } from './context/PortfolioContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PortfolioProvider>
-      <App />
-    </PortfolioProvider>
+    <BrowserRouter basename="/mypersonalportfolio">
+      <PortfolioProvider>
+        <App />
+      </PortfolioProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
